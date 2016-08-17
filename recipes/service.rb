088afde37f01b1da7530882a467ef7740f0e50ea
Artur Melo <artur.melo@beubi.com>
2016-08-17 22:42:04 +0100

    Use 'systemd' instead of 'upstart' as service provider for Ubuntu 15.04+

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,6 +1,10 @@
 # use upstart on ubuntu > saucy
 service_provider = Chef::Provider::Service::Upstart if 'ubuntu' == node['platform'] &&
   Chef::VersionConstraint.new('>= 13.10').include?(node['platform_version'])
+  
+if 'ubuntu' == node['platform'] && Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version'])
+  service_provider = Chef::Provider::Service::Systemd
+end
 
 # this just reloads the dnsmasq rules when the template is adjusted
 service 'lxc-net' do
